9d9afa0c8438c822e3045d98f3ed28e2775010f4
“0rainge” <Orange96@aliyun.com>
2019-02-01 21:45:48 +0800

    fix(bug): the position of Record's key

--- accounts-app/src/components/Records.js
+++ accounts-app/src/components/Records.js
@@ -29,7 +29,7 @@ class Records extends Component {
         </thead>
         <tbody>
           {/* <Record/> */}
-          {this.state.records.map((record) => <Record record={record} / >)}
+          {this.state.records.map((record) => <Record key ={record.id} record={record} / >)}
         </tbody>
         </table>
 
