9e6e8132deaa715df0367cedecf3c7943e5273ce
“0rainge” <Orange96@aliyun.com>
2019-02-02 03:18:55 +0800

    feat(Records): set state: err and isLoaded; get data from mockAPI by jquery

--- accounts-app/src/components/Records.js
+++ accounts-app/src/components/Records.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
 import Record from './Record';
+import $ from 'jquery';
  
 
 class Records extends Component {
   constructor(){
     super();
     this.state = {
+      err:null,
+      isLoaded:false,
       records:[
-        {"id":1,"date":"2019-02-01","title":"敲代码的收入","amount":0.5},
-        {"id":2,"date":"2019-01-28","title":"买小鱼干的支出","amount":10.0},
-        {"id":3,"date":"2019-01-22","title":"出去浪的支出","amount":5.0},
+        // {"id":1,"date":"2019-02-01","title":"敲代码的收入","amount":0.5},
+        // {"id":2,"date":"2019-01-28","title":"买小鱼干的支出","amount":10.0},
+        // {"id":3,"date":"2019-01-22","title":"出去浪的支出","amount":5.0},
       ]
     }
   }
 
+  componentDidMount(){
+    $.getJSON("http://5c544a2ea659410014eeeb2c.mockapi.io/api/v1/records").then(
+      response => this.setState({
+        isLoaded:true,
+        records: response
+      }),
+      err => this.setState({
+        isLoaded:true,
+        err//相当于err:err
+        
+        
+      })
+    )
+  }
+
 
   render() {
+    const {err,isLoaded,records} = this.state;
+    if(err)
+      return <div>啊偶出现了bug！{err.responseText}</div>;
+    else if(!isLoaded){
+      return <div>正在加载大猫咪的小金库</div>
+    }else{
+
+    }
+
+
     return (
       <div >
         <h2>大猫咪的小账本</h2>
